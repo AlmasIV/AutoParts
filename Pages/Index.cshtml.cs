500b3eb0bac20f349420f2ad482798b673d137f2
AlmasIV <Almas.Kairbek.AK@gmail.com>
2023-09-25 20:48:20 +0600

    Working on the table of auto parts.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using AutoParts.Model;
+using System.Text.Json;
 
 namespace AutoParts.Pages;
 
@@ -10,6 +11,7 @@ public class IndexModel : PageModel
     private readonly DatabaseAcess _databaseAccess;
     [BindNever]
     public List<AutoPart>? AutoParts { get; set; } = null;
+    public List<string> JsonAutoParts { get; set; } = new List<string>();
     public IndexModel(DatabaseAcess databaseAcess)
     {
         _databaseAccess = databaseAcess;
@@ -20,6 +22,9 @@ public class IndexModel : PageModel
         (bool isSuccess, List<AutoPart>? autoParts) outputs = _databaseAccess.RetrieveAll();
         if(outputs.isSuccess){
             AutoParts = outputs.autoParts;
+            foreach(AutoPart autoPart in AutoParts){
+                JsonAutoParts.Add(JsonSerializer.Serialize(autoPart));
+            }
             return Page();
         }
         return File("~/html/ErrorPages/serverError.html", "text/html");
