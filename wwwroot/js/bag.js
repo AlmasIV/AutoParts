b29a40d24ab350d74c86d46f68787772c58bb9f9
AlmasIV <Almas.Kairbek.ak@gmail.com>
2023-10-12 19:07:27 +0600

    Working on the list display.

--- wwwroot/js/bag.js
+++ wwwroot/js/bag.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const bag = new Map();
 let itemCount = 0;
 
 let product, isCreated = false;
@@ -17,15 +16,8 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 
 function addToBag(id){
-    product = { id: id, amount: 1 };
-    if(bag.has(product.id)){
-        bag.get(product.id).amount++;
-    }
-    else{
-        bag.set(product.id, product);
-    }
     itemCount ++;
-    UpdateProductCount(id);
+    updateProductCount(id);
 }
 
 const productCount = document.getElementById("bagCount");
@@ -34,33 +26,87 @@ const itemsInfo = document.getElementById("itemsInfo");
 
 const detailedProducts = new Map();
 
-function UpdateProductCount(id){
+function updateProductCount(id){
     productCount.textContent = itemCount;
     if(itemCount > 0){
         productsDesc.textContent = "Selling " + itemCount + " items in total.";
-        let tr, name, applicability, company, price;
-        bag.forEach(function(value, key, bag){
-            tr = document.querySelector(`main#autoPartsTable table tr[data-item-id="${key}"]`);
-            name = tr.querySelector("td[headers='name']");
-            applicability = tr.querySelector("td[headers='applicability']");
-            company = tr.querySelector("td[headers='company']");
-            price = tr.querySelector("td[headers='priceInTenge']");
-            if(!detailedProducts.has(key)){
-                detailedProducts.add(key, { name, applicability, company, price, amount: value.amount });
-            }
-        });
+
+        if(!detailedProducts.has(id)){
+            let tr, name, applicability, company, price;
+            tr = document.querySelector(`main#autoPartsTable table tr[data-item-id="${id}"]`);
+            name = tr.querySelector("td[headers='name']").textContent;
+            applicability = tr.querySelector("td[headers='applicability']").textContent;
+            company = tr.querySelector("td[headers='company']").textContent;
+            price = tr.querySelector("td[headers='priceInTenge']").textContent;
+            detailedProducts.set(id, { name, applicability, company, price, amount: 1 });
+            createProduct(id);
+        }
+        else{
+            detailedProducts.get(id).amount ++;
+            displayProductAmount(id);
+        }
     }
     else{
         productsDesc.textContent = "You have no products here.";
     }
 }
 
+const createdProducts = new Map();
+
+function createProduct(id){
+    let detailedProduct = detailedProducts.get(id);
+
+    let container = document.createElement("div");
+    container.classList.add("productContainer");
+    container.id = "product_" + id;
+    
+    let name = document.createElement("p");
+    name.classList.add("productName");
+    name.textContent = "Name: " + detailedProduct.name;
+    container.appendChild(name);
+
+    let applicability = document.createElement("p");
+    applicability.classList.add("productApplicability");
+    applicability.textContent = "Applicability: " + detailedProduct.applicability;
+    container.appendChild(applicability);
+
+    let company = document.createElement("p");
+    company.classList.add("productCompany");
+    company.textContent = "Company: " + detailedProduct.company;
+    container.appendChild(company);
+
+    let price = document.createElement("p");
+    price.classList.add("productPrice");
+    price.textContent = "Price: " + detailedProduct.price;
+    container.appendChild(price);
+
+    let amount = document.createElement("p");
+    amount.classList.add("productAmount");
+    amount.textContent = "Amount: " + detailedProduct.amount;
+    container.appendChild(amount);
+
+    createdProducts.set(id, container);
+
+    itemsInfo.appendChild(container);
+}
+function displayProductAmount(id){
+    createdProducts.get(id).querySelector("p.productAmount").textContent = "Amount: " + detailedProducts.get(id).amount;
+}
+
+function removeProductFromList(){
+
+}
+
 const bagBtn = document.getElementById("bag");
 
 let isBagBtnActive = false;
+let timeOutId;
 
-bagBtn.addEventListener("mouseover", showProductsDesc);
+bagBtn.addEventListener("mouseover", function(){
+    timeOutId = setTimeout(showProductsDesc, 500);
+});
 bagBtn.addEventListener("mouseout", function(){
+    clearTimeout(timeOutId);
     if(!isBagBtnActive){
         hideProductsDesc();
     }
@@ -80,7 +126,9 @@ bagBtn.addEventListener("click", function(){
 
 function showProductsDesc(){
     productsDesc.style.display = "block";
+    itemsInfo.style.display = "block";
 }
 function hideProductsDesc(){
     productsDesc.style.display = "none";
+    itemsInfo.style.display = "none";
 }
